b031aa8149d4e996bf484ed198904d25f0b72db7
Tuomas <c0ritu00@students.oamk.fi>
2021-04-06 13:24:49 +0300

    all persons works

--- qt_frontend/mainwindow.cpp
+++ qt_frontend/mainwindow.cpp
@@ -16,7 +16,7 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_btnGetAllPersons_clicked()
 {
-    QString site_url="http://localhost:3000/example/persons";
+    QString site_url="http://localhost:3000/example/allpersons";
     QString credentials="automat123:pass123";
     QNetworkRequest request((site_url));
     request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
@@ -33,7 +33,15 @@ void MainWindow::allPersonsSlot(QNetworkReply * reply)
 {
     QByteArray response_data=reply->readAll();
     qDebug()<<response_data;
-
+    QJsonDocument json_doc=QJsonDocument::fromJson(response_data);
+    QJsonArray json_array = json_doc.array();
+    QString persons;
+    foreach(const QJsonValue &value, json_array){
+        QJsonObject json_obj=value.toObject();
+        persons+=QString::number((json_obj["id_person"].toInt()))+" "+json_obj["fname"].toString()+"\r\n";
+    }
+    ui->labelResult->setText(persons);
+    allPersonsReply->deleteLater();
     reply->deleteLater();
     allPersonsManager->deleteLater();
 }
